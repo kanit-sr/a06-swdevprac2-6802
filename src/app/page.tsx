bdc7911b528ed84faf109413994c487598472389
Kanit <stu33074@gmail.com>
2026-03-15 21:36:40 +0700

    add: CardPanel.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Banner from "@/components/Banner";
-import Card from "@/components/Card";
+import CardPanel from "@/components/CardPanel";
 
 export default function Home() {
   return (
     <main>
       <Banner />
-      <div className="flex flex-row flex-wrap justify-center gap-6 pb-8 px-4" style={{ marginTop: '3rem', marginBottom: '3rem' }}>
-        <Card venueName="The Grand Table" imgSrc="/img/grandtable.jpg" />
-        <Card venueName="Spark Space" imgSrc="/img/sparkspace.jpg" />
-        <Card venueName="The Bloom Pavilion" imgSrc="/img/bloom.jpg" />
-      </div>
+      <CardPanel />
     </main>
   );
 }
